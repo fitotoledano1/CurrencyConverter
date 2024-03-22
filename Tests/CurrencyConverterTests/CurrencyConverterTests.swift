595957e1207c74e8c80a525666dc56f0fce72f6b
Fito Toledano <fito.toledano@gmail.com>
2024-03-22 17:27:16 +0100

    Initial Commit

---
+++ Tests/CurrencyConverterTests/CurrencyConverterTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import CurrencyConverter
+
+final class CurrencyConverterTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
