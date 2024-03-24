1ea80fa668d7142081598c758173ce83bd809d8c
Fito Toledano <fito.toledano@gmail.com>
2024-03-24 23:07:31 +0100

    update package configuration

--- Package.swift
+++ Package.swift
@@ -5,17 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "CurrencyConverter",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CurrencyConverter",
             targets: ["CurrencyConverter"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/fitotoledano1/LantanaNetwork",
+            branch: "main"
+        ),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CurrencyConverter"),
+            name: "CurrencyConverter",
+            dependencies: ["LantanaNetwork"]
+        ),
         .testTarget(
             name: "CurrencyConverterTests",
             dependencies: ["CurrencyConverter"]),
