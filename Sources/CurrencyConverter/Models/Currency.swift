b4747e8fff4be10e0c4fb7642138d7e816b25cb7
Fito Toledano <fito.toledano@gmail.com>
2024-03-24 23:37:54 +0100

    Refactor Currency enum

---
+++ Sources/CurrencyConverter/Models/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  
+//
+//  Created by Fito Toledano on 24/03/2024.
+//
+
+import Foundation
+
+public enum Currency: String, Decodable {
+    case usd = "USD"
+    case eur = "EUR"
+}
